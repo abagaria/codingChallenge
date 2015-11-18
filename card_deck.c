55de9ecbe1d6ee03e5fb3cd46b715d9ea770a84e
AkhilBagaria <akhilbagaria@WL-204-66.WPA.Claremont.Edu>
2015-11-18 01:03:41 -0800

    * Added function structures * Functions just return their default return values

---
+++ card_deck.c
@@ -0,0 +1,30 @@
+#import "card_deck.h"
+
+deck_t make_deck(int num_cards) {
+	deck_t deck;
+	return deck;
+}
+
+void move_to_top(deck_t *handdeck, deck_t *tabledeck) {
+	return;
+}
+
+void move_to_bottom(deck_t *handdeck) {
+	return;
+}
+
+int is_equal(deck_t original_deck, deck_t tabledeck) {
+	return 1;
+}
+
+void play_round(deck_t *handdeck, deck_t *tabledeck) {
+	return;
+}
+
+void print(deck_t deck) {
+	return;
+}
+
+int main(void) {
+	return 1;
+}
