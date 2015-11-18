ade8da48074dd823be03a8273133b52d682bf7d7
AkhilBagaria <akhilbagaria@AkhilBagarias-MacBook-Pro.local>
2015-11-18 01:52:06 -0800

    Added print functions for cards and decks. Also fixed bug in card def

--- card_deck.c
+++ card_deck.c
@@ -21,10 +21,56 @@ void play_round(deck_t *handdeck, deck_t *tabledeck) {
 	return;
 }
 
+/*
+*	Print function for a deck. Iterate through the cards 
+*	in the deck, and print the identity of the cards to stdout
+*/
 void print(deck_t deck) {
+	card_t *current = deck.top;
+	
+	if (current == NULL)
+		return;
+
+	printf("%d ->", current->identity);
+	while (current->next != NULL) {
+		current = current->next;
+		printf(" %d ->", current->identity);
+	}
+	printf("\n");
+	return;
+}
+
+/*
+*	Print function for a card so that top and bottom can easily be printed.
+*/
+void print_card(card_t *card) {
+	if (card == NULL) {
+		return;
+	}
+
+	printf("%d\n", card->identity);
 	return;
 }
 
 int main(void) {
+	// make 2 cards, one on top of each other
+	card_t topcard;
+	topcard.identity = 1;
+
+	card_t nextcard;
+	nextcard.identity = 2;
+
+	topcard.next = &nextcard;
+	nextcard.next = NULL;
+
+	// create deck from the 2 cards above
+	deck_t deck;
+	deck.num_cards = 2;
+	deck.top = &topcard;
+	deck.bottom = &nextcard;
+
+	// print this deck
+	print(deck);
+
 	return 1;
 }
